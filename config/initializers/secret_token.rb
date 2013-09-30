6546263e32221652adf1665881a354a7a020dfff
Eyal Kornblut <eyal.kornblut@gmail.com>
2013-09-30 19:15:43 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EvaBatya::Application.config.secret_key_base = '7ab9d9c8ea28351cb22492efb84b3d18d85b28c8fe2c7f316e2e23327d6003179a955425e588d61e06ea45186d6202be57dccb7d6701b4d40faa76320242a42c'
